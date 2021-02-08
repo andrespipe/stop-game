1c02682d7bd945d071e8cec4aea4a4b58736ca1f
andresm <andresm@blanclabs.com>
2021-02-08 18:38:16 -0500

    initial commit

---
+++ apps/stop-game-next/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  displayName: 'stop-game-next',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': [
+      'babel-jest',
+      { cwd: __dirname, configFile: './babel-jest.config.json' },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/stop-game-next',
+};
