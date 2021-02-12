93d22a0d155b1091b73cba9c9ca380bea14e86f0
andresm <andresm@blanclabs.com>
2021-02-11 22:31:43 -0500

    Material UI added

--- apps/stop-game-next/next.config.js
+++ apps/stop-game-next/next.config.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
-
+// const withCSS = require('@zeit/next-css');
+// module.exports = withCSS({})
 module.exports = withNx({});
--- apps/stop-game-next/pages/_app.tsx
+++ apps/stop-game-next/pages/_app.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import MainToolbar from '@stop-game-next-organisms/mainToolbar/mainToolbar';
 import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
-import './styles.css';
+// import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,13 +12,14 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <title>Welcome to stop-game-next!</title>
       </Head>
       <div className="app">
-        <header className="flex">
+        <MainToolbar></MainToolbar>
+        {/* <header className="flex">
           <NxLogo width="75" height="50" />
           <h1>Welcome to stop-game-next!</h1>
         </header>
         <main>
           <Component {...pageProps} />
-        </main>
+        </main> */}
       </div>
     </>
   );
