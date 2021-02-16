863241d823ad8c46ab2083011808aae5ff3c6c07
andresm <andresm@blanclabs.com>
2021-02-15 22:44:34 -0500

    Home link

--- apps/stop-game-next/components/organisms/mainToolbar/mainToolbar.tsx
+++ apps/stop-game-next/components/organisms/mainToolbar/mainToolbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,7 +7,8 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import './mainToolbar.module.scss';
+import ChildCareIcon from '@material-ui/icons/ChildCare';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -32,9 +34,20 @@ export default function MainToolbar() {
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
+          <div className={classes.title}>
+            <Link href="/" passHref>
+              <Button color="inherit">
+                <Grid container direction="row" alignItems="center">
+                  <Grid item>
+                    <ChildCareIcon fontSize="large" />
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="h6" className={classes.title}>Stop game</Typography>
+                  </Grid>
+                </Grid>
+              </Button>
+            </Link>
+          </div>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
